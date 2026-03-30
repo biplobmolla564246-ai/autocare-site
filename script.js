32ce13909b77ced4d7732d267c4bcbe2a728e8cf
biplobmolla564246-ai <biplobmolla564246@gmail.com>
2026-03-30 12:31:13 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ counters.forEach(counter => {
 
   updateCounter();
 });
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
